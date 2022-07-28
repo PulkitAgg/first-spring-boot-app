db77f47cf2d5519ee7124a7f174241851ecd0801
Pulkit Aggarwal <pulkit.aggarwal@nagarro.com>
2022-07-28 19:19:26 +0530

    adding dependency injection without any config

---
+++ src/main/java/com/example/demo/FirstSpringBootAppApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class FirstSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(FirstSpringBootAppApplication.class, args);
+		System.out.println("Welocme to boot...");
+		Alien a = context.getBean(Alien.class);
+		a.show();
+	}
+
+}
